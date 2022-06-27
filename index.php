502da1fb8acbf4ce98b5e700b2e7af2384c5e2e0
Rod-Ritter <rodolphe.ritter@gmail.com>
2022-06-27 09:59:34 +0200

    gettype

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$toto = "<h1>12</h1>";
+echo $toto;
+
+$jour = "lundi";
+echo 'nous sommes ' . $jour ;
+echo "<br/>";
+echo 'nous sommes $jour';
+
+echo gettype($jour);
+
+
+// remplace console log
+var_dump($jour);
+
+
